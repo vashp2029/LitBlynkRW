d63433d28738d3a6af245f8981c3ba1e605cc1e3
Vash Patel <vashp2029@yahoo.com>
2018-04-03 21:10:34 -0400

    Reserved selectedLedGroup value "1" for global

--- src/SetSpecific.h
+++ src/SetSpecific.h
@@ -4,32 +4,32 @@
   #ifdef BED
     #define SENSORNAME "Bed"
     #define NUMLEDS 150
-    #define LEDGROUP 1
+    #define LEDGROUP 2 //LEDGROUP 1 is reserved as "global" or "all groups"
 
   #elif COUCH
     #define SENSORNAME "Couch"
     #define NUMLEDS 150
-    #define LEDGROUP 2
+    #define LEDGROUP 3
 
   #elif TV
     #define SENSORNAME "TV"
     #define NUMLEDS 150
-    #define LEDGROUP 3
+    #define LEDGROUP 4
 
   #elif TVSTAND
     #define SENSORNAME "TV Stand"
     #define NUMLEDS 150
-    #define LEDGROUP 4
+    #define LEDGROUP 5
 
   #elif WALLDESK
     #define SENSORNAME "Wall Desk"
     #define NUMLEDS 150
-    #define LEDGROUP 5
+    #define LEDGROUP 6
 
   #elif GLASSDESK
     #define SENSORNAME "Glass Desk"
     #define NUMLEDS 150
-    #define LEDGROUP 6
+    #define LEDGROUP 7
 
   #endif
 
